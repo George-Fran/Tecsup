58e9554a7089e01b9b01254972aaeae64d85d80b
George-Fran <george.gutierrez@tecsup.edu.pe>
2023-10-30 18:35:07 -0500

    whitenoise

--- Tecsup/settings.py
+++ Tecsup/settings.py
@@ -127,11 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "backend", "static"),
-]
 
 
 LOGIN_REDIRECT_URL = 'dashboard'
@@ -139,6 +135,14 @@ LOGOUT_REDIRECT_URL = 'home'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+STATIC_URL = 'static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    
 
 LOGIN_URL = 'login'
 
