2f53bddd8108fb44073041cd9ca38968b20f4193
George-Fran <george.gutierrez@tecsup.edu.pe>
2023-10-30 21:18:09 -0500

    proyectos

--- backend/views.py
+++ backend/views.py
@@ -47,10 +47,12 @@ def dashboard(request):
 
     noticias = Noticia.objects.order_by('-id_noticia')[:2]
     eventos = Evento.objects.order_by('-id_evento')[:2]
+	proyectos = Proyecto.objects.order_by('-id_proyecto')[:2]
 
     context = {
         'noticias': noticias,
         'eventos': eventos,
+		'proyectos': proyectos,
         'cantidad_noticias': cantidad_noticias,
         'cantidad_eventos': cantidad_eventos,
         'cantidad_proyectos': cantidad_proyectos
